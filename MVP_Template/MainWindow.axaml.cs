3b9f79ba8dfd4c87900106ca5d193ac7d62271cc
joaoVLAndrade <joaoandrade.dtna@gmail.com>
2026-02-11 13:47:30 -0300

    Initial commit

---
+++ MVP_Template/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MVP_Template
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVP_Template/src/Shared/DTOs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVP_Template.src.Shared
+{
+    internal class DTOs
+    {
+    }
+}
